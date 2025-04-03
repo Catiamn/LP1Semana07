b31c676c5853f1d119c9f0626d8f30d819ee3ff0
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-04-03 16:57:32 +0100

    Added red, green and blue

---
+++ ColorSpheres/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ColorSpheres
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            public class Color
+            {
+                private byte red
+                {
+                    get
+                    {
+                        return red
+                    }
+                    set 
+                    {
+                        byte red = true
+                    }
+                }
+                private byte green
+                {
+                    get
+                    {
+                        return green
+                    }
+                    set 
+                    {
+                        byte green = true
+                    }
+                }
+                private byte blue
+                {
+                    get
+                    {
+                        return blue
+                    }
+                    set 
+                    {
+                        byte blue = true
+                    }
+                }
+            }
+
+            private class Sphere(Color, raio, ThrowTimes)
+            {
+                int raio = 0
+            }
+
+            
+        }
+    }
+}

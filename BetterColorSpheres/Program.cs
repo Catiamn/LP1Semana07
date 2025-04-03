f236874f6ad41c6f7feeaa1d25c99493cbb63bb8
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-04-03 23:10:35 +0100

    Added Class Color

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -4,9 +4,25 @@ namespace BetterColorSpheres
 {
     public class Program
     {
-        private static void Main(string[] args)
+        public class Color
         {
-            Console.WriteLine("Hello LP1!");
+            //Propriedades!
+            public byte Red { get; }
+            public byte Green { get; }
+            public byte Blue { get; }
+
+
+            //Construtor
+            public Color(byte red, byte green, byte blue)
+            {
+                Red = red;
+                Green = green;
+                Blue = blue;
+            }
+
+            //readonly (read only)
+             public byte Grey => (byte)((Red + Green + Blue) / 3);
         }
+
     }
 }
